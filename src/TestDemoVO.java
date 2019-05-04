51cef00842f532d03c14985357ec50c1178aa753
fengjin1988 <465342124@qq.com>
2019-05-04 23:04:41 +0800

    编译通过

--- src/TestDemoVO.java
+++ src/TestDemoVO.java
@@ -3,4 +3,21 @@
  * Create by Administrator 2019/5/4 16:01
  */
 public class TestDemoVO {
+    public static void main(String[] args) {
+        if (args == null) {
+            
+        }
+
+        System.out.println();
+
+        System.out.printf("");
+
+        System.out.println("TestDemoVO.main");
+
+        System.out.println("args = [" + args + "]");
+
+        System.out.println("false = " + false);
+
+        System.out.println();
+    }
 }

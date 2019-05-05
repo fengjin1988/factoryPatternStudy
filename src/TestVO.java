4d803d03fcd9baf68914a615e813da58d2058a4a
fengjin1988 <465342124@qq.com>
2019-05-05 21:31:00 +0800

    test

--- src/TestVO.java
+++ src/TestVO.java
@@ -3,5 +3,31 @@
  * Create by Administrator 2019/5/5 21:07
  */
 public class TestVO {
+    private String name;
+    private Integer age;
+    private String address;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+/*    public void setAddress(String address) {
+        this.address = address;
+    }*/
 }
